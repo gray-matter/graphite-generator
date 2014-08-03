bf1924ac72a38794c67a051fa35ddc09cb568ed6
Xavier NOELLE <xavier.noelle@gmail.com>
2014-08-03 21:15:16 +0200

    Implement a first version of the verbose mode

--- lib/board-generator.rb
+++ lib/board-generator.rb
@@ -1,9 +1,10 @@
 require 'conf-reader'
 
 class BoardGenerator
-  def initialize(conf_reader, graphite_server)
+  def initialize(conf_reader, graphite_server, verbose)
     @conf_reader = conf_reader
     @graphite_server = graphite_server
+    @verbose = verbose
   end
 
   def generate(conf_file_path, expression_checker)
@@ -12,8 +13,33 @@ class BoardGenerator
     end
 
     dashboards = @conf_reader.parse(conf_file_path, graphite_expression_checker)
+    reverse_index = dashboards.size()
     dashboards.each() do |dashboard|
+      reverse_index -= 1
+
+      if @verbose
+        pretty_print_dasboard(dashboard)
+        puts if reverse_index > 0
+      end
+
       dashboard.save!(@graphite_server)
+    end
+  end
+
+  def pretty_print_dasboard(dashboard)
+    puts "- #{dashboard.name}"
+
+    dashboard.graphs.each() do |graph|
+      # FIXME when https://github.com/criteo/graphite-dashboard-api/pull/1 is merged
+      puts "-- #{graph.to_hash['title']}"
+
+      graph.targets.each() do |target|
+        puts "--- #{target}"
+      end
+
+      graph.extra_options.each() do |opt_key, opt_value|
+        puts "--- #{opt_key}: #{opt_value}"
+      end
     end
   end
 end
